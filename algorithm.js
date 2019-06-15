9c576c4d8f86ab5c748659ef257859ee744fa58e
Mohamed Anwar <moanwar1997@gmail.com>
2019-06-15 22:25:49 +0200

    fixed return commuliation

--- algorithm.js
+++ algorithm.js
@@ -51,13 +51,14 @@ module.exports = {
     for (var i = 0; i < arr.length; i++)
       up[i] = arr[i] > 0 ? (N * T) / arr[i] : N;
     
-        var result = ans;
-        ans = [];
-    if (solve(0, N, N * T)) {
-      print(0, N, N * T);
-      return result;
-    } else return [-1];
         
+      var result = -1;
+        if (solve(0, N, N * T)) {
+            print(0, N, N * T);
+            result = ans;
+        }
+        ans = [];
+        return result;
   }
 };
 //Write the test case
