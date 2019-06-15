6568c36bc1d0381a360c3d1e0b2c38acf9f627ed
Mohamed Anwar <moanwar1997@gmail.com>
2019-06-15 22:27:08 +0200

    fixed syntax error

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-// const algorithm = require('./algorithm');
+const algorithm = require('./algorithm');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/", function (req, res) {
-    var algorithm = require('./algorithm');
     res.writeHead(200, { "Content-Type": "application/json" });
     array = req.body.i;
     n = req.body.n;
     t = req.body.t;
     ans = algorithm.RUN(array, n, t);
-    res.end(JSON.stringify({ result: ans });
+    res.end(JSON.stringify({ result: ans }));
 });
 
 app.listen(process.env.PORT || 8001, function() {
